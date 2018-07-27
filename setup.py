67e3c0cb91c3d033ce4e6e65592638684f7c4e30
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-27 17:45:17 +0530

    Bump patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='grtracer',
-    version='1.2.0',
+    version='1.2.1',
     description='Simple tracing setup using Jaeger and Flask',
     url='https://github.com/ryzokuken/grtracer',
     author='Ujjwal Sharma',
