0084a06e54a06f38f7a02acdccbc81b137e1d09f
Ujjwal Sharma <usharma1998@gmail.com>
2018-07-30 13:57:00 +0530

    Add flask submodule

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='grtracer',
-    version='1.2.2',
+    version='1.2.3',
     description='Simple tracing setup using Jaeger and Flask',
     url='https://github.com/ryzokuken/grtracer',
     author='Ujjwal Sharma',
     author_email='usharma1998@gmail.com',
     license='MIT',
-    packages=['grtracer'],
+    packages=['grtracer', 'grtracer.flask'],
     install_requires=[
         'flask==0.10.1',
         'flask_opentracing==0.2.0',
